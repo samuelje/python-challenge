74d9e45904ec9e339ad64f117772f6a1bfff4b45
samuelje <jeannanicole1020@gmail.com>
2021-03-24 21:30:57 -0600

    comments and updates to PyBank code added

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,45 +1,52 @@
-#The total number of months included in the dataset
-#The net total amount of "Profit/Losses" over the entire period
-#Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
-
-#Modules
+#Import modules
 import os
 import csv
 
-#CSV path and open/read CSV
+#csv path
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
-#with open(csvpath) as csvfile:
-    
-#    csvreader = csv.reader(csvfile, delimiter=',')
+#Create empty lists to iterate through specific rows for a given variable
+total_months = []
+total_profit = []
+monthly_profit_change = []
 
-#   print(csvreader)
-
-#    csv_header = next(csvreader)
-#    print(f"CSV Header: {csv_header}")
-
-#    for row in csvreader:
- #       print(row)
-
-#lists to store data
-date = []
-#month = []
-#year = []
-#Profits = []
-#Losses = []
-
+#Open/read csv file
 with open(csvpath, newline='') as csvfile:
+
+#   Create csvreader variable to store contents of budget.csv file
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    for row in csvreader:
-#       Split date to get month only separated from year
-        date.append(row[0])
-        month_only = row[0].split("-")
-#        print(month_only)
-        total_months = len(list(month_only))
-        print(total_months)
-       
+#   Skip header labels in order to interpret values
+    header = next(csvreader)
 
-#Final outputs
+#   Iterate through the rows in the csvreader variables stored contents
+    for row in csvreader:
+
+#       Split date to get month only separated from year
+        total_months.append(row[0])
+        total_profit.append(int(row[1]))
+
+#   Loop through the profits to get monthly change in profits
+    for i in range(len(total_profit)-1):
+
+#       Find difference between given two months and append to monthly profit change list
+        monthly_profit_change.append(total_profit[i+1] - total_profit[i])
+
+#Calculate max and min from monthly profit change list
+max_increase_value = max(monthly_profit_change)
+max_decrease_value = min(monthly_profit_change)
+
+#Correlate max and min values to proper month list and index
+#Use + 1 to associate change with "next" month
+max_increase_month = monthly_profit_change.index(max(monthly_profit_change)) + 1
+max_decrease_month = monthly_profit_change.index(min(monthly_profit_change)) + 1
+
+#Final statements
+
+print("Financial Analysis")
+print("--------------------------")
+print(f"Total Months: {len(total_months)}")
+print(f"Total: ${sum(total_profit)}")
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change), 2)}")
+print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")

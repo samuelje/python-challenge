3c9cd72b4b81654ed9e34be43ef512bad4fb6275
samuelje <jeannanicole1020@gmail.com>
2021-03-26 19:49:44 -0600

    PyBank txt file updates added

--- PyBank/main.py
+++ PyBank/main.py
@@ -50,3 +50,24 @@ print(f"Total: ${sum(total_profit)}")
 print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change), 2)}")
 print(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
 print(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
+#Output file path
+output_file = os.path.join('analysis', 'PyBank_Analysis_Summary.txt')
+
+with open(output_file, "w") as file:
+
+#   Print final statements to PyBank_Analysis_Summary
+    file.write("Financial Analysis")
+    file.write("\n")
+    file.write("--------------------------")
+    file.write("\n")
+    file.write(f"Total Months: {len(total_months)}")
+    file.write("\n")
+    file.write(f"Total: ${sum(total_profit)}")
+    file.write("\n")
+    file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change), 2)}")
+    file.write("\n")
+    file.write(f"Greatest Increase in Profits: {total_months[max_increase_month]} (${(str(max_increase_value))})")
+    file.write("\n")
+    file.write(f"Greatest Decrease in Profits: {total_months[max_decrease_month]} (${(str(max_decrease_value))})")
+
